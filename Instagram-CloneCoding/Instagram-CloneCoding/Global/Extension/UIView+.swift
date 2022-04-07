6b6473a8d31fd83faaa0cd1418837bc05cf4f1bc
eunjuchoi <eunjuzzoo@naver.com>
2022-04-07 23:03:04 +0900

    ➕[ADD] Extension 파일 추가 (#1)

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UIView+.swift
@@ -0,0 +1,50 @@
+//
+//  UIView+.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/07.
+//
+
+import UIKit
+
+extension UIView {
+    
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+    var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+    
+    /// 다수의 뷰를 한번에 addSubview해주는 메서드
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { self.addSubview($0) }
+    }
+    
+    /// UIView 의 모서리가 둥근 정도를 설정하는 메서드
+    func makeRounded(cornerRadius: CGFloat?) {
+        if let cornerRadius_ = cornerRadius {
+            self.layer.cornerRadius = cornerRadius_
+        }  else {
+            // cornerRadius 가 nil 일 경우의 default
+            self.layer.cornerRadius = self.layer.frame.height / 2
+        }
+        
+        self.layer.masksToBounds = true
+    }
+    
+    /**
+     - Description:
+     VC나 View 내에서 해당 함수를 호출하면, 햅틱이 발생하는 메서드입니다.
+     버튼을 누르거나 유저에게 특정 행동이 발생했다는 것을 알려주기 위해 다음과 같은 햅틱을 활용합니다.
+     
+     - parameters:
+     - degree: 터치의 세기 정도를 정의. 보통은 medium,light를 제일 많이 활용합니다.
+     */
+    func makeVibrate(degree : UIImpactFeedbackGenerator.FeedbackStyle = .medium)
+    {
+        let generator = UIImpactFeedbackGenerator(style: degree)
+        generator.impactOccurred()
+    }
+}
---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UIViewController+.swift
@@ -0,0 +1,34 @@
+//
+//  UIViewController+.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/07.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+    var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+    
+    /**
+     - Description: 화면 터치시 키보드 내리는 Extension
+     */
+    /// 화면 터치시 키보드 내리는 메서드
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
