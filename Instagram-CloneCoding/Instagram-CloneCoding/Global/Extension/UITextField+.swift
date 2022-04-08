0f084cc35bf79ec3a643ee66c5bc8be3d13caac7
eunjuchoi <eunjuzzoo@naver.com>
2022-04-09 01:51:08 +0900

    ➕[ADD] textField 관련 extension 추가 (#2)

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UITextField+.swift
@@ -0,0 +1,42 @@
+//
+//  UITextField+.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/09.
+//
+
+import Foundation
+
+import UIKit
+
+extension UITextField {
+    
+    /// UITextField의 상태를 리턴함
+    var isEmpty: Bool {
+        if text?.isEmpty ?? true {
+            return true
+        }
+        return false
+    }
+    
+    /// UITextField 왼쪽에 여백 주는 메서드
+    func addLeftPadding(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    /// UITextField 오른쪽에 여백 주는 메서드
+    func addRightPadding(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+    
+    /// 자간 설정 메서드
+    func setCharacterSpacing(_ spacing: CGFloat){
+        let attributedStr = NSMutableAttributedString(string: self.text ?? "")
+        attributedStr.addAttribute(NSAttributedString.Key.kern, value: spacing, range: NSMakeRange(0, attributedStr.length))
+        self.attributedText = attributedStr
+     }
+}
