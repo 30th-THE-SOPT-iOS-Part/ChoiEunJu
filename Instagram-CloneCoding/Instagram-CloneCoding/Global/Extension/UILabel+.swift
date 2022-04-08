fa1bb1923f47ef6c78a0c36d8ee55703f16e4d2f
eunjuchoi <eunjuzzoo@naver.com>
2022-04-08 12:26:14 +0900

    ➕[ADD] UILabel 관련 extension 추가 (#2)

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UILabel+.swift
@@ -0,0 +1,41 @@
+//
+//  UILabel+.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/08.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    /// 행간 조정 메서드
+    func setLineSpacing(lineSpacing: CGFloat) {
+        if let text = self.text {
+            let attributedStr = NSMutableAttributedString(string: text)
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = lineSpacing
+            attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, attributedStr.length))
+            self.attributedText = attributedStr
+        }
+    }
+    
+    /// 자간 설정 메서드
+    func setCharacterSpacing(_ spacing: CGFloat) {
+        let attributedStr = NSMutableAttributedString(string: self.text ?? "")
+        attributedStr.addAttribute(NSAttributedString.Key.kern, value: spacing, range: NSMakeRange(0, attributedStr.length))
+        self.attributedText = attributedStr
+    }
+    
+    /// 자간과 행간을 모두 조정하는 메서드
+    func setLineAndCharacterSpacing(lineSpacing: CGFloat, characterSpacing: CGFloat) {
+        if let text = self.text {
+            let attributedStr = NSMutableAttributedString(string: text)
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = lineSpacing
+            attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, attributedStr.length))
+            attributedStr.addAttribute(NSAttributedString.Key.kern, value: characterSpacing, range: NSMakeRange(0, attributedStr.length))
+            self.attributedText = attributedStr
+        }
+    }
+}
