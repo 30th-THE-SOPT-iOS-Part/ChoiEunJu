adecd900e341798b573017b7a9f6e180cde3c819
eunjuchoi <eunjuzzoo@naver.com>
2022-04-08 17:33:44 +0900

    ✨[FEAT] 비밀번호 만들기 뷰 UI 구성 및 화면전환 구현

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Screen/SignUp/Controller/MakePasswordViewController.swift
@@ -0,0 +1,117 @@
+//
+//  MakePasswordViewController.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/08.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MakePasswordViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let backBtn = UIButton().then {
+        $0.setImage(UIImage(named: "icn_back_24"), for: .normal)
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "비밀번호 만들기"
+        $0.setCharacterSpacing(1.2)
+        $0.font = .systemFont(ofSize: 24, weight: .semibold)
+        $0.textAlignment = .center
+        $0.sizeToFit()
+    }
+    
+    private let subTitleLabel = UILabel().then {
+        $0.text = "비밀번호를 저장할 수 있으므로 iCloud 기기에서 로그인\n정보를 입력하지 않아도 됩니다."
+        $0.textColor = .gray
+        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        $0.sizeToFit()
+    }
+    
+    private let userNameTextField = UITextField().then {
+        $0.placeholder = "비밀번호"
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+        $0.borderStyle = .roundedRect
+    }
+    
+    private let nextBtn = UIButton().then {
+        $0.setTitle("다음", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .systemBlue
+        $0.makeRounded(cornerRadius: 5.adjusted)
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        setUpTapBackBtn()
+        setUpTapNextBtn()
+    }
+}
+
+// MARK: - UI
+extension MakePasswordViewController {
+    private func configureUI() {
+        view.addSubviews([backBtn, titleLabel, subTitleLabel, userNameTextField, nextBtn])
+        
+        backBtn.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(60)
+            $0.leading.equalToSuperview().offset(12)
+            $0.width.height.equalTo(32)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(90)
+            $0.centerX.equalToSuperview()
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(15)
+            $0.centerX.equalTo(titleLabel.snp.centerX)
+        }
+        
+        userNameTextField.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(45)
+        }
+        
+        nextBtn.snp.makeConstraints {
+            $0.top.equalTo(userNameTextField.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(50)
+        }
+    }
+}
+
+// MARK: - Custom Methods
+extension MakePasswordViewController {
+    
+    /// 뒤로가기 버튼 tap Action 설정 메서드
+    private func setUpTapBackBtn() {
+        backBtn.press {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    /// 다음 버튼 tap Action 설정 메서드
+    private func setUpTapNextBtn() {
+        nextBtn.press {
+            guard let nextViewController = UIStoryboard.init(name: "SignInStoryBoard", bundle: nil).instantiateViewController(withIdentifier: ResultViewController.className) as? ResultViewController else { return }
+            
+            nextViewController.modalPresentationStyle = .fullScreen
+            self.present(nextViewController, animated: true, completion: nil)
+            self.navigationController?.popToRootViewController(animated: true)
+        }
+    }
+}
+
