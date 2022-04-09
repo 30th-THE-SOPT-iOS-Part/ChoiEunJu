0c188237c5da404c54166ef18f4d879234225e66
eunjuchoi <eunjuzzoo@naver.com>
2022-04-09 13:14:02 +0900

    ➕[ADD] Identifiers 생성 (#2)

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Struct/Identifiers.swift
@@ -0,0 +1,13 @@
+//
+//  Identifiers.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/09.
+//
+
+import Foundation
+
+struct Identifiers {
+    static let SignInStoryBoard = "SignInStoryBoard"
+    static let SignUpStoryBoard = "SignUpStoryBoard"
+}
