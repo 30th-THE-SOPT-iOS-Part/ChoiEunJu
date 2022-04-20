d8d0017f1f5d9926c83d9760fb07f13a3a334524
eunjuchoi <eunjuzzoo@naver.com>
2022-04-21 01:06:38 +0900

    ➕[ADD] : UIColor+ 추가 (#4)

---
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UIColor+.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+.swift
+//  Instagram-CloneCoding
+//
+//  Created by EUNJU on 2022/04/21.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var darkGray: UIColor {
+        return UIColor(red: 114.0 / 255.0, green: 114.0 / 255.0, blue: 114.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var lightGray: UIColor {
+        return UIColor(red: 250.0 / 255.0, green: 250.0 / 255.0, blue: 250.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var btnBlue: UIColor {
+        return UIColor(red: 55.0 / 255.0, green: 151.0 / 255.0, blue: 239.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var strokeBlack: UIColor {
+        return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.1)
+    }
+}
