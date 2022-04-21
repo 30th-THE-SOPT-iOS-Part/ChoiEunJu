65fb7681a4f98044400505a0f4b01be6add89212
eunjuchoi <eunjuzzoo@naver.com>
2022-04-22 03:00:58 +0900

    ✅[CHORE] : 이미지 renderingMode 수정 (#4)

--- Instagram-CloneCoding/Instagram-CloneCoding/Screen/TabBar/Controller/InstagramTabBarController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/Screen/TabBar/Controller/InstagramTabBarController.swift
@@ -18,20 +18,20 @@ class InstagramTabBarController: UITabBarController {
 
 // MARK: - UI
 extension InstagramTabBarController {
-    func makeTabBar(vc: UIViewController , tabBarImg: String, tabBarSelectedImg: String) -> UIViewController {
+    func makeTabBar(vc: UIViewController , tabBarImg: String, tabBarSelectedImg: String, renderingMode: UIImage.RenderingMode) -> UIViewController {
 
         let tab = UINavigationController(rootViewController: vc)
-        tab.tabBarItem = UITabBarItem(title: "", image: UIImage(named: tabBarImg)?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: tabBarSelectedImg)?.withRenderingMode(.alwaysOriginal))
+        tab.tabBarItem = UITabBarItem(title: "", image: UIImage(named: tabBarImg)?.withRenderingMode(renderingMode), selectedImage: UIImage(named: tabBarSelectedImg)?.withRenderingMode(renderingMode))
         
         return tab
     }
     
     func configureTabBar() {
-        let homeTab = makeTabBar(vc: HomeViewController(), tabBarImg: "icn_home", tabBarSelectedImg: "icn_home_selected")
-        let searchTab = makeTabBar(vc: SearchViewController(), tabBarImg: "icn_search", tabBarSelectedImg: "icn_search_selected")
-        let videoTab = makeTabBar(vc: VideoViewController(), tabBarImg: "icn_reels", tabBarSelectedImg: "icn_reels_selected")
-        let shopTab = makeTabBar(vc: ShopViewController(), tabBarImg: "icn_shop", tabBarSelectedImg: "icn_shop_selected")
-        let mypageTab = makeTabBar(vc: MypageViewController(), tabBarImg: "icn_profile", tabBarSelectedImg: "icn_profile_selected")
+        let homeTab = makeTabBar(vc: HomeViewController(), tabBarImg: "icn_home", tabBarSelectedImg: "icn_home_selected", renderingMode: .alwaysTemplate)
+        let searchTab = makeTabBar(vc: SearchViewController(), tabBarImg: "icn_search", tabBarSelectedImg: "icn_search_selected", renderingMode: .alwaysTemplate)
+        let videoTab = makeTabBar(vc: VideoViewController(), tabBarImg: "icn_reels", tabBarSelectedImg: "icn_reels_selected", renderingMode: .alwaysTemplate)
+        let shopTab = makeTabBar(vc: ShopViewController(), tabBarImg: "icn_shop", tabBarSelectedImg: "icn_shop_selected", renderingMode: .alwaysTemplate)
+        let mypageTab = makeTabBar(vc: MypageViewController(), tabBarImg: "icn_profile", tabBarSelectedImg: "icn_profile_selected", renderingMode: .alwaysOriginal)
         
         /// 탭 구성
         let tabs = [homeTab, searchTab, videoTab, shopTab, mypageTab]
