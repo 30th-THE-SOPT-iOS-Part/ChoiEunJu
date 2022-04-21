4faa7717a0023e337e6eef284e78c7aaf89d2506
eunjuchoi <eunjuzzoo@naver.com>
2022-04-21 14:02:05 +0900

    ✅[CHORE] : 완료화면 코드 수정 (#4)

--- Instagram-CloneCoding/Instagram-CloneCoding/Screen/SignIn/Controller/ResultViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/Screen/SignIn/Controller/ResultViewController.swift
@@ -16,27 +16,24 @@ class ResultViewController: UIViewController {
     
     private let messageLabel = UILabel().then {
         $0.text = "000님, Instagram에\n오신 것을 환영합니다"
-        $0.font = .systemFont(ofSize: 22, weight: .regular)
+        $0.font = .SFProTextR(size: 24)
         $0.numberOfLines = 2
         $0.textAlignment = .center
         $0.sizeToFit()
-        $0.setLineAndCharacterSpacing(lineSpacing: 4, characterSpacing: 1)
+        $0.setLineAndCharacterSpacing(lineSpacing: 4, characterSpacing: 0.15)
     }
     
     private let descriptionLabel = UILabel().then {
         $0.text = "언제든지 연락처 정보와 사용자 이름을 변경할 수 있습니다."
-        $0.textColor = .gray
-        $0.font = .systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = .darkGray
+        $0.font = .SFProTextR(size: 12)
         $0.textAlignment = .center
         $0.sizeToFit()
     }
     
-    private let completeBtn = UIButton().then {
+    private let completeBtn = InstagramBtn().then {
         $0.setTitle("완료하기", for: .normal)
-        $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
-        $0.setTitleColor(.white, for: .normal)
-        $0.backgroundColor = .systemBlue
-        $0.makeRounded(cornerRadius: 5.adjusted)
+        $0.isActivated = true
     }
     
     // MARK: - Life Cycle
@@ -54,20 +51,20 @@ extension ResultViewController {
         view.addSubviews([messageLabel, descriptionLabel, completeBtn])
         
         messageLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(300)
+            $0.top.equalToSuperview().inset(310)
             $0.centerX.equalToSuperview()
         }
         
         descriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(messageLabel.snp.bottom).offset(14)
+            $0.top.equalTo(messageLabel.snp.bottom).offset(23)
             $0.centerX.equalTo(messageLabel.snp.centerX)
         }
         
         completeBtn.snp.makeConstraints {
-            $0.top.equalTo(descriptionLabel.snp.bottom).offset(26)
-            $0.leading.equalToSuperview().offset(20)
-            $0.trailing.equalToSuperview().offset(-20)
-            $0.height.equalTo(50)
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(23)
+            $0.leading.equalToSuperview().inset(38)
+            $0.trailing.equalToSuperview().inset(37)
+            $0.height.equalTo(40)
         }
     }
 }
