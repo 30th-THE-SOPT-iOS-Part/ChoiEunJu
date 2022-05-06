c88e3955ca409957a1009efb06bfe70ede3eeb40
eunjuchoi <eunjuzzoo@naver.com>
2022-05-06 13:28:33 +0900

    ✨[FEAT] : 기본 TableView UI 구성 (#6)

--- Instagram-CloneCoding/Instagram-CloneCoding/Screen/Home/Controller/HomeViewController.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/Screen/Home/Controller/HomeViewController.swift
@@ -31,18 +31,27 @@ class HomeViewController: UIViewController {
     private let directMessageBtn = UIButton().then {
         $0.setImage(UIImage(named: "icn_share"), for: .normal)
     }
+    
+    private let postTableView = UITableView().then {
+        $0.separatorColor = .clear
+    }
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        registerCell()
+        setUpTableView()
     }
 }
 
 // MARK: - UI
 extension HomeViewController {
     private func configureUI() {
-        view.addSubviews([naviView])
+        view.backgroundColor = .white
+        postTableView.backgroundColor = .white
+        
+        view.addSubviews([naviView, postTableView])
         naviView.addSubviews([logoImgView, addPostBtn, likeListBtn, directMessageBtn])
         
         naviView.snp.makeConstraints {
@@ -74,5 +83,44 @@ extension HomeViewController {
             $0.bottom.equalTo(likeListBtn.snp.bottom)
             $0.width.height.equalTo(24)
         }
+        
+        postTableView.snp.makeConstraints {
+            $0.top.equalTo(naviView.snp.bottom)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+// MARK: - Custom Methods
+extension HomeViewController {
+    private func registerCell() {
+        postTableView.register(HomePostTableViewCell.self, forCellReuseIdentifier: HomePostTableViewCell.className)
+    }
+    
+    /// TableView setting 함수
+    private func setUpTableView() {
+        postTableView.delegate = self
+        postTableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PostDataModel.dummyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomePostTableViewCell.className, for: indexPath) as? HomePostTableViewCell else { return UITableViewCell() }
+        
+        cell.setData(model: PostDataModel.dummyData[indexPath.row])
+        return cell
     }
 }
