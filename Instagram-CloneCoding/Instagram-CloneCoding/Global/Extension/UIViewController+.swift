f34fcc39773c98f3ef3a3fb47eef4ffcc014db64
eunjuchoi <eunjuzzoo@naver.com>
2022-05-13 14:26:03 +0900

    ➕[ADD] : alert 생성 메서드 추가 (#8)

--- Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UIViewController+.swift
+++ Instagram-CloneCoding/Instagram-CloneCoding/Global/Extension/UIViewController+.swift
@@ -31,4 +31,31 @@ extension UIViewController {
     func dismissKeyboard() {
         view.endEditing(true)
     }
+    
+    /// 확인 버튼 Alert 메서드
+    func makeAlert(title : String, message : String? = nil,
+                   okTitle: String = "확인", okAction : ((UIAlertAction) -> Void)? = nil,
+                   completion : (() -> Void)? = nil) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        let okAction = UIAlertAction(title: okTitle, style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+    
+    /// 확인 버튼 누르면 화면전환되는 Alert 메서드
+    func makePresentAlert(title : String, message : String? = nil,
+                   okTitle: String = "확인", nextVC: UIViewController) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        alertViewController.addAction(UIAlertAction(title: okTitle, style: .default) { action in
+            nextVC.modalPresentationStyle = .fullScreen
+            self.present(nextVC, animated: true)
+        })
+        self.present(alertViewController, animated: true, completion: nil)
+    }
 }
